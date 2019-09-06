9d0a82c8a6308de025f5147e021f39597361581a
zanzan13531 <zanzan13531@gmail.com>
2019-09-06 11:57:01 -0700

    added hello_world and my_name
    hello_world just prints hello world
    my_name just prints some stuff as well

---
+++ AP_CS_stuff/src/AA_hello_world/hello_world.java
@@ -0,0 +1,11 @@
+package AA_hello_world;
+
+public class hello_world {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+}
---
+++ AP_CS_stuff/src/AA_hello_world/my_name.java
@@ -0,0 +1,13 @@
+package AA_hello_world;
+
+public class my_name {
+	
+	public static void main(String[] args){
+		
+		System.out.println("******************");
+		System.out.println("**my name is bob**");
+		System.out.println("******************");
+		
+	}
+	
+}
