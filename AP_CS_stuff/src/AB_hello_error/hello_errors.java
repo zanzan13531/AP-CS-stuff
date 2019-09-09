d0f56c904f8bb0cb3e9de8340c9d42a89c8519ab
zanzan13531 <zanzan13531@gmail.com>
2019-09-09 08:13:04 -0700

    added hello_errors and hello_errors_fixed

---
+++ AP_CS_stuff/src/AB_hello_error/hello_errors.java
@@ -0,0 +1,15 @@
+package AB_hello_error;
+
+public class hello_errors {
+	
+	public static void main(String[] args)	{
+		
+	    System.out.println("Hello World");
+	    System.out.println("There is an error here");
+	    System.out.print("There is an error here");
+	    System.out.println(" too");
+	    System.out.println();
+	    
+	}
+	
+}
