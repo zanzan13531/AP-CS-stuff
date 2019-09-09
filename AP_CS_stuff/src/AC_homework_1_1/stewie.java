42d90edcfd7a54c8f86bf41ec3d0fde0e7b9f16b
zanzan13531 <zanzan13531@gmail.com>
2019-09-09 08:25:29 -0700

    added 1.1 homework
    added stewie.java which is runnable in eclipse, and added submitted.txt
    which is the text file that was pasted into the buildci318.com site

---
+++ AP_CS_stuff/src/AC_homework_1_1/stewie.java
@@ -0,0 +1,20 @@
+package AC_homework_1_1;
+
+public class stewie {
+	
+	public static void main(String[] args)	{
+		
+		/* print this stuff
+		 * //////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+		 */
+		
+		
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		
+	}
+	
+}
