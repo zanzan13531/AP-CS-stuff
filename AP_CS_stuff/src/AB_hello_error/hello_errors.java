4266245898cba4ef2335c287f455cc3ffee78418
zanzan13531 <zanzan13531@gmail.com>
2019-09-09 08:15:08 -0700

    switched the code in hello_errors and hello_errors_fixed

--- AP_CS_stuff/src/AB_hello_error/hello_errors.java
+++ AP_CS_stuff/src/AB_hello_error/hello_errors.java
@@ -2,14 +2,16 @@ package AB_hello_error;
 
 public class hello_errors {
 	
-	public static void main(String[] args)	{
-		
-	    System.out.println("Hello World");
-	    System.out.println("There is an error here");
-	    System.out.print("There is an error here");
-	    System.out.println(" too");
-	    System.out.println();
-	    
+	/*
+	 *code provided
+	public static void main(String[] args)
+	{
+	    System.out.println("Hello World);
+	    System.out.printl("There is an error here")
+	    system.out.print(There is an error here);
+	    System.out.println("too");
+	    system.out.println()
 	}
+	*/
 	
 }
--- AP_CS_stuff/src/AB_hello_error/hello_errors_fixed.java
+++ AP_CS_stuff/src/AB_hello_error/hello_errors_fixed.java
@@ -2,14 +2,13 @@ package AB_hello_error;
 
 public class hello_errors_fixed {
 	
-	/*
-	public static void main(String[] args)
-	{
-	    System.out.println("Hello World);
-	    System.out.printl("There is an error here")
-	    system.out.print(There is an error here);
-	    System.out.println("too");
-	    system.out.println()
+public static void main(String[] args)	{
+		
+	    System.out.println("Hello World");
+	    System.out.println("There is an error here");
+	    System.out.print("There is an error here");
+	    System.out.println(" too");
+	    System.out.println();
+	    
 	}
-	*/
 }
