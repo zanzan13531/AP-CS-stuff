13c1fe91c3abec1e878226d9deac1d6f77c184e1
zanzan13531 <zanzan13531@gmail.com>
2019-09-09 13:10:01 -0700

    initial start of exersize 1.5

---
+++ AP_CS_stuff/src/AD_unit_1_homework_1_1_exersize_1_5/much_better.java
@@ -0,0 +1,25 @@
+package AD_unit_1_homework_1_1_exersize_1_5;
+
+public class much_better {
+
+	/* text provided
+	 * A "quoted" String is
+'much' better if you learn
+the rules of "escape sequences."
+Also, "" represents an empty String.
+Don't forget: use \" instead of " !
+'' is not the same as "
+	 */
+	
+	public static void main(String[] args)	{
+		
+		System.out.println("A \"quoted\" String is");
+		System.out.println("'much' better if you learn");
+		System.out.println("the rules of \"escape sequences.\"");
+		System.out.println("Also, \"\" represents an empty String.");
+		System.out.println("Don't forget: use \\\" instead of \" !");
+		System.out.println("'' is not the same as \"");
+		
+	}
+	
+}
