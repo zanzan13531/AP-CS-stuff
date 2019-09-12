aa9d5ae9d2975fcfb346ce02cca85883416e135c
zanzan13531 <zanzan13531@gmail.com>
2019-09-12 09:37:58 -0700

    added stewie2 thingy

---
+++ AP_CS_stuff/src/AE_unit_1_homework_1_2_exersize_1_8/stewie2.java
@@ -0,0 +1,41 @@
+package AE_unit_1_homework_1_2_exersize_1_8;
+
+public class stewie2 {
+
+	public static void slashes() {
+
+		System.out.println("//////////////////////");
+
+	}
+
+	public static void victory() {
+
+		System.out.println("|| Victory is mine! ||");
+
+	}
+
+	public static void backslashes() {
+
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+	}
+	
+	public static void sequence() {
+		
+		victory();
+		backslashes();
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		slashes();
+		sequence();
+		sequence();
+		sequence();
+		sequence();
+		sequence();
+		
+	}
+
+}
