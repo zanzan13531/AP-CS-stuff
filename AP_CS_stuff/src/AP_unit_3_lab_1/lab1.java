c703ca48eec44b2110b33c35da3c322851f823a6
zanzan13531 <zanzan13531@gmail.com>
2019-09-26 08:08:02 -0700

    added unit 3 labs 1 and 2

---
+++ AP_CS_stuff/src/AP_unit_3_lab_1/lab1.java
@@ -0,0 +1,21 @@
+package AP_unit_3_lab_1;
+
+public class lab1 {
+    
+    public static void draw(int n) {
+        
+        for(int i = 0; i < n; i++) {
+            
+            System.out.print("*");
+            
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        draw(10);
+        
+    }
+    
+}
