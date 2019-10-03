88adf0262eb850b23dbfcc50fadd055a0b60b187
zanzan13531 <zanzan13531@gmail.com>
2019-10-03 09:42:50 -0700

    unit 4 homeowrk 4.2 part 1/4, also made reverestring2 which is only one line long

---
+++ AP_CS_stuff/src/AW_unit_4_homework_4_1_self_check_3_19/reversestring2.java
@@ -0,0 +1,1 @@
+package AW_unit_4_homework_4_1_self_check_3_19; public class reversestring2 { public static String reversifystring(String s) { String a = "";for (int x = 0; x < s.length(); x++) {a += s.substring(s.length()-(x+1),s.length()-x);}return(a);}public static void main(String[] args) {System.out.println(reversifystring("asdfasdf"));}}
