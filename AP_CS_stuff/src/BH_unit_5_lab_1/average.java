523cddebaa5d940da16c401c3b942ae3c3d9912b
zanzan13531 <zanzan13531@gmail.com>
2019-10-20 14:28:05 -0700

    unit 5, lab part 1 done

---
+++ AP_CS_stuff/src/BH_unit_5_lab_1/average.java
@@ -0,0 +1,34 @@
+package BH_unit_5_lab_1;
+
+import java.util.*;
+
+public class average {
+	
+	public static void main(String[] args) {
+		
+		int newest = 0;
+		int sum = 0;
+		double stuffs = 0.0;
+		
+		Scanner bob = new Scanner(System.in);
+		
+		System.out.println("Enter a number:");
+		newest = Integer.parseInt(bob.nextLine());
+		stuffs = stuffs + 1;
+		
+		while (newest != -1) {
+			
+			sum = sum + newest;
+			
+			System.out.println("Enter a number:");
+			newest = Integer.parseInt(bob.nextLine());
+			stuffs = stuffs + 1;
+			
+		}
+		
+		stuffs = stuffs - 1;
+		System.out.println("Average: " + sum/stuffs);
+		
+	}	
+	
+}
