18654ddbc165c82510dd16acb5ed505f625d33f4
zanzan13531 <zanzan13531@gmail.com>
2020-03-03 12:22:37 -0800

    stuffs

---
+++ AP_CS_stuff/src/CA_reverseInsertionSort/reverseInsertionSort.java
@@ -0,0 +1,17 @@
+package CA_reverseInsertionSort;
+
+public class reverseInsertionSort {
+
+	public static void main(String[] args) {
+		
+		int[] bob = new int[] {6,8,2,7,1,4};
+		
+		for (int x = 0; x < bob.length; x++) {
+			
+			
+			
+		}
+		
+	}
+	
+}
